ae3c4ff50182cf27ae7823309240fb6c8242c4fb
Diegor-mtz <diegoregalado.martinez@gmail.com>
2025-01-07 18:48:07 +0000

    Añadir demo de Streamlit y reporte de leyes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+@st.cache
+def load_data():
+    return pd.read_csv('Reporte_Arreglado.csv')
+
+data = load_data()
+
+st.title('Reporte de Aparición de Dependencias en Leyes')
+
+leyes = st.multiselect('Selecciona la Ley', data['doc_name'].unique())
+dependencias = st.multiselect('Selecciona la Dependencia', data['entity_text'].unique())
+
+filtered_data = data
+if leyes:
+    filtered_data = filtered_data[filtered_data['doc_name'].isin(leyes)]
+if dependencias:
+    filtered_data = filtered_data[filtered_data['entity_text'].isin(dependencias)]
+
+st.dataframe(filtered_data)
